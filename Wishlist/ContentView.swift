bc07bd44916baf1c3bdf18bab851a5b1126b5f62
Jourdese Palacio <jourdesepalacio@gmail.com>
2025-08-27 13:25:33 +0800

    Model Context

--- Wishlist/ContentView.swift
+++ Wishlist/ContentView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
+    @Environment(\.modelContext) private var modelContext
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
