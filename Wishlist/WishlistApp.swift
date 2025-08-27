024ce4e236f2e37bd6e28aecabcb9361b05e8281
Jourdese Palacio <jourdesepalacio@gmail.com>
2025-08-27 12:43:32 +0800

    Initial Commit

---
+++ Wishlist/WishlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  WishlistApp.swift
+//  Wishlist
+//
+//  Created by Jourdese Palacio on 8/27/25.
+//
+
+import SwiftUI
+
+@main
+struct WishlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
