b994a1c92082081bafada35aab97f18cb58615b1
Jourdese Palacio <jourdesepalacio@gmail.com>
2025-08-27 13:33:50 +0800

    Query Macro

--- Wishlist/ContentView.swift
+++ Wishlist/ContentView.swift
@@ -10,18 +10,21 @@ import SwiftData
 
 struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
+    @Query private var wishes: [Wish]
     
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(wishes) {
+                    wish in Text(wish.title)
+                }
+            }
+                .navigationTitle("Wishlist")
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+        .modelContainer(for: Wish.self, inMemory: true)
 }
